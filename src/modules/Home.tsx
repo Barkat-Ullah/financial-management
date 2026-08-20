97c0553bf44c3611d40cf524183fc0ddff196802
Barkat-Ullah <barkatullah585464@gmail.com>
2026-08-20 20:37:54 +0600

    feature : add a global section and testimonial section

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -1,12 +1,16 @@
 import WhyChooseUs from "./pages/WhyChoose";
 import HowItWorks from "./pages/HowWorks";
 import Features from "./pages/Features";
+import GlobalSection from "./pages/GlobalSection";
+import Testimonial from "./pages/Testimonial";
 
 const HomePage = () => {
     return <>
         <WhyChooseUs />
         <HowItWorks />
         <Features />
+        <GlobalSection />
+        <Testimonial /> 
     </>;
 };
 
